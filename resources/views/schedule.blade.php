8323140a748302f7fc97b8309e17c4270c2cc4a2
dever4eg <dever4eg@gmail.com>
2018-02-13 23:28:51 +0200

    add s3 storage

--- resources/views/schedule.blade.php
+++ resources/views/schedule.blade.php
@@ -2,22 +2,17 @@
 
 @section('content')
 
-    <img class="img-responsive" src="http://gatisnau.sumy.ua/images/rozklad/rozklad4.png" alt="">
+    <!-- <img class="img-responsive" src="http://gatisnau.sumy.ua/images/rozklad/rozklad4.png" alt="">
     <img class="img-responsive" src="http://gatisnau.sumy.ua/images/rozklad/rozklad3.png" alt="">
     <img class="img-responsive" src="http://gatisnau.sumy.ua/images/rozklad/rozklad2.png" alt="">
     <img class="img-responsive" src="http://gatisnau.sumy.ua/images/rozklad/rozklad1.png" alt="">
     <img class="img-responsive" src="http://gatisnau.sumy.ua/images/rozklad/rozklad.png" alt="">
+ -->
 
-
-    @forelse($types as $item)
-        <hp class="text-center"> {{ $item->type_name }} </hp>
-    @empty
-        <h3>Записи отсутствуют</h3>
-    @endforelse
 
     @forelse($schedules as $item)
      <h2 class="text-center"> {{ $item->type->type_name }} : {{ $item->date }} </h2>
-        <img class="img-responsive" src="storage/{{ $item->path_img }}" alt="">
+        <img class="img-responsive" src="{{ Storage::url($item->path_img) }}" alt="">
     @empty
         <h3>Записи отсутствуют</h3>
     @endforelse
